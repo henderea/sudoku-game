dff7597cc847a72c729273ccff053fc18459f0cc
Eric Henderson <henderea@gmail.com>
2023-01-18 17:53:55 -0500

    fix import order

--- lib/game/NumberBar.tsx
+++ lib/game/NumberBar.tsx
@@ -1,7 +1,7 @@
-import { _times } from 'lib/util/general';
 import type { JSX } from 'solid-js';
 
 import { selection } from './grid-state';
+import { _times } from 'lib/util/general';
 
 function NumberCell(props: { value: number }): JSX.Element {
   return <td class={(props.value % 2 == 0) ? 'even' : 'odd'} classList={{ selected: props.value == selection.get() }} onClick={[selection.set, props.value]}>{props.value}</td>;
