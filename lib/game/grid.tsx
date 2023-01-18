4a1b331d8cbf3892bd3345d86db4c099f259f526
Eric Henderson <henderea@gmail.com>
2023-01-18 16:01:13 -0500

    add the initial grid element

---
+++ lib/game/grid.tsx
@@ -0,0 +1,22 @@
+import type { JSX } from 'solid-js';
+
+import { Show } from 'solid-js';
+import Cell from './cell';
+import { timer } from './timer';
+import { _times } from 'lib/util/general';
+
+export default function Grid(): JSX.Element {
+  return (
+    <Show when={timer.running()} fallback={<div class="paused-grid"><div class="paused-grid-icon"></div></div>}>
+      <table class="grid">
+        {_times(9, (row: number) => (
+          <tr>
+            {_times(9, (column: number) => (
+              <Cell index={row * 9 + column}/>
+            ))}
+          </tr>
+        ))}
+      </table>
+    </Show>
+  );
+}
