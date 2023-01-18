99348022102cabb44b95b6e28fd5ef9de0935577
Eric Henderson <henderea@gmail.com>
2023-01-18 14:07:45 -0500

    re-arrange the way the difficulty types and constants are defined

--- lib/sudoku/difficulty.ts
+++ lib/sudoku/difficulty.ts
@@ -2,16 +2,12 @@ function r(a: number, b: number): [number, number] {
   return [a, b];
 }
 
-export type DifficultyMap<T> = {
-    easy: T,
-    medium: T,
-    hard: T
-};
-
-export const difficultySpaces: DifficultyMap<[number, number]> = {
+export const difficultySpaces = {
   easy: r(35, 45),
   medium: r(30, 35),
   hard: r(25, 30)
-};
+} as const;
 
-export declare type Difficulty = keyof DifficultyMap<any>;
+export declare type Difficulty = keyof typeof difficultySpaces;
+export declare type DifficultyMap<T> = Record<Difficulty, T>;
+export const difficulties: Difficulty[] = Object.keys(difficultySpaces) as Difficulty[];
