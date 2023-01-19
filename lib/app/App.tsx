42a452e73b09ffaa3096de67732d25b86aea20c5
Eric Henderson <henderea@gmail.com>
2023-01-19 12:29:29 -0500

    move this to the Game screen

--- lib/app/App.tsx
+++ lib/app/App.tsx
@@ -1,15 +1,10 @@
 import type { JSX } from 'solid-js';
 
 import { Dynamic } from 'solid-js/web';
-import { onMount } from 'solid-js';
 
-import { timer } from './game/TimerDisplay';
 import { menus, menuType } from './menu-state';
 
 export default function App(): JSX.Element {
-  onMount(() => {
-    timer.startTimerInterval();
-  });
   return (
     <>
       <Dynamic component={menus[menuType.get()].menu}/>
--- lib/app/game/Game.tsx
+++ lib/app/game/Game.tsx
@@ -1,10 +1,17 @@
 import type { JSX } from 'solid-js';
 
+import { onMount } from 'solid-js';
+
+import { timer } from './TimerDisplay';
+
 import Header from './Header';
 import Grid from './Grid';
 import NumberBar from './NumberBar';
 
 export default function Game(): JSX.Element {
+  onMount(() => {
+    timer.startTimerInterval();
+  });
   return (
     <>
       <Header/>
