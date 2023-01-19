810b291428de897efbb83c72432dde91b007b4bb
Eric Henderson <henderea@gmail.com>
2023-01-19 14:03:49 -0500

    support storing play counts

--- lib/util/Storage.ts
+++ lib/util/Storage.ts
@@ -1,37 +1,63 @@
-import { Difficulty, DifficultyMap, makeDifficultyMap } from 'lib/sudoku/difficulty';
+import type { Difficulty, DifficultyMap } from 'lib/sudoku/difficulty';
+import type { KeysOfType } from './general';
+
+import { makeDifficultyMap } from 'lib/sudoku/difficulty';
 
 export interface Storage {
   get bestTimes(): DifficultyMap<number>;
   set bestTimes(value: DifficultyMap<number>);
 
+  get playCounts(): DifficultyMap<number>;
+  set playCounts(value: DifficultyMap<number>);
+
   setBestTime(difficulty: Difficulty, millis: number): Storage;
   updateBestTime(difficulty: Difficulty, millis: number): Storage;
 
+  incrementPlayCount(difficulty: Difficulty): Storage;
+
   loadTimes(): Storage;
   saveTimes(): Storage;
+
+  loadPlayCounts(): Storage;
+  savePlayCounts(): Storage;
 }
 
 const TIMES_STORAGE_KEY: string = 'game-times';
+const PLAY_COUNTS_STORAGE_KEY: string = 'game-play-counts';
+
+// True to automatically load/save before/after each operation (method)
+const AUTO_LOAD_AND_SAVE: boolean = true;
 
 const emptyDifficultyMap = () => makeDifficultyMap(() => 0);
 
 class StorageImpl implements Storage {
   private _bestTimes: DifficultyMap<number> = emptyDifficultyMap();
+  private _playCounts: DifficultyMap<number> = emptyDifficultyMap();
 
   get bestTimes(): DifficultyMap<number> { return this._bestTimes; }
   set bestTimes(value: DifficultyMap<number>) { this._bestTimes = value; }
+  get playCounts(): DifficultyMap<number> { return this._playCounts; }
+  set playCounts(value: DifficultyMap<number>) { this._playCounts = value; }
 
   setBestTime(difficulty: Difficulty, millis: number): Storage {
+    this.autoLoad('loadTimes');
     this.bestTimes[difficulty] = millis;
-    return this;
+    return this.autoSave('saveTimes');
   }
 
   updateBestTime(difficulty: Difficulty, millis: number): Storage {
+    this.autoLoad('loadTimes');
     const currentBest: number = this.bestTimes[difficulty];
     if(currentBest <= 0 || currentBest > millis) {
-      this.setBestTime(difficulty, millis);
+      this.bestTimes[difficulty] = millis;
     }
-    return this;
+    return this.autoSave('saveTimes');
+  }
+
+  incrementPlayCount(difficulty: Difficulty): Storage {
+    this.autoLoad('loadPlayCounts');
+    this.playCounts[difficulty] = this.playCounts[difficulty] + 1;
+    return this.autoSave('savePlayCounts');
   }
 
   private load<K extends keyof this>(key: K, storageKey: string, parse: (data: string) => this[K], fallback: () => this[K]): this {
@@ -54,8 +80,25 @@ class StorageImpl implements Storage {
     return this;
   }
 
+  private autoLoad<K extends KeysOfType<StorageImpl, () => Storage> & `load${string}`>(key: K): this {
+    if(AUTO_LOAD_AND_SAVE) { this[key](); }
+    return this;
+  }
+
+  private autoSave<K extends KeysOfType<StorageImpl, () => Storage> & `save${string}`>(key: K): this {
+    if(AUTO_LOAD_AND_SAVE) { this[key](); }
+    return this;
+  }
+
   loadTimes(): Storage { return this.load('bestTimes', TIMES_STORAGE_KEY, JSON.parse, emptyDifficultyMap); }
   saveTimes(): Storage { return this.save('bestTimes', TIMES_STORAGE_KEY, JSON.stringify); }
+
+  loadPlayCounts(): Storage { return this.load('playCounts', PLAY_COUNTS_STORAGE_KEY, JSON.parse, emptyDifficultyMap); }
+  savePlayCounts(): Storage { return this.save('playCounts', PLAY_COUNTS_STORAGE_KEY, JSON.stringify); }
+
+  autoLoadAll(): this {
+    return this.autoLoad('loadTimes').autoLoad('loadPlayCounts');
+  }
 }
 
-export const storage: Storage = new StorageImpl();
+export const storage: Storage = new StorageImpl().autoLoadAll();
